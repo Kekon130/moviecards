0f3a44720f28e0ea0eb60c3104bdafbf727e051e
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2026-02-03 23:50:06 +0100

    Cambios en main para adaprtar el uso del servicio remoto

--- src/main/java/com/lauracercas/moviecards/service/movie/MovieServiceImpl.java
+++ src/main/java/com/lauracercas/moviecards/service/movie/MovieServiceImpl.java
@@ -1,9 +1,11 @@
 package com.lauracercas.moviecards.service.movie;
 
-
 import com.lauracercas.moviecards.model.Movie;
 import com.lauracercas.moviecards.repositories.MovieJPA;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,25 +17,29 @@ import java.util.List;
 @Service
 public class MovieServiceImpl implements MovieService {
 
-    private final MovieJPA movieJPA;
-
-    public MovieServiceImpl(MovieJPA movieJPA) {
-        this.movieJPA = movieJPA;
-    }
-
+    @Autowired
+    private RestTemplate template;
+    private String url = "https://moviecards-service-plaza.azurewebsites.net/movies";
 
     @Override
     public List<Movie> getAllMovies() {
-        return movieJPA.findAll();
+        Movie[] moviesArray = template.getForObject(url, Movie[].class);
+        return List.of(moviesArray);
     }
 
     @Override
     public Movie save(Movie movie) {
-        return movieJPA.save(movie);
+        if (movie.getId() != null && movie.getId() > 0) {
+            this.template.put(url, movie);
+        } else {
+            movie.setId(0);
+            this.template.postForObject(url, movie, String.class);
+        }
+        return movie;
     }
 
     @Override
     public Movie getMovieById(Integer movieId) {
-        return movieJPA.getById(movieId);
+        return template.getForObject(url + "/" + movieId, Movie.class);
     }
 }
